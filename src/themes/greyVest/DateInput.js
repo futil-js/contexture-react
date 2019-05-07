52cd675a7f1270af384173e551f8def9b01b3875
Ilian Alexiev <ialexiev@smartprocure.us>
2019-05-06 17:53:00 -0700

    Fix dateInput to work with Date vs String

---
+++ src/themes/greyVest/DateInput.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { observer } from 'mobx-react'
+import DatePicker from 'react-date-picker'
+
+let DateInput = observer(({ value, calendarIcon=null, clearIcon=null,...props }) => (
+  <DatePicker 
+    calendarType={'US'}
+    calendarIcon={calendarIcon}
+    clearIcon={clearIcon}
+    value={_.isDate(value) ? value : new Date(value)}
+    {...props}
+  />
+))
+export default DateInput
