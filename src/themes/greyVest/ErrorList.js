b9daeb73aa80320ffe042a2abfe30227cd5b51ed
Decrapifier <decrapifier@govspend.us>
2019-06-07 16:51:47 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti

--- src/themes/greyVest/ErrorList.js
+++ src/themes/greyVest/ErrorList.js
@@ -3,12 +3,15 @@ import ErrorBlock from './ErrorBlock'
 import ErrorText from './ErrorText'
 
 let ErrorList = ({ block = false, children, ...props }) =>
-  React.Children.map(children,
-    e =>
-      block ? (
-        <ErrorBlock key={e} {...props}>{e}</ErrorBlock>
-      ) : (
-        <ErrorText key={e} {...props}>{e}</ErrorText>
-      ),
+  React.Children.map(children, e =>
+    block ? (
+      <ErrorBlock key={e} {...props}>
+        {e}
+      </ErrorBlock>
+    ) : (
+      <ErrorText key={e} {...props}>
+        {e}
+      </ErrorText>
+    )
   ) || null
 export default ErrorList
--- src/themes/greyVest/ErrorText.js
+++ src/themes/greyVest/ErrorText.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 // Error Text / List General Components
 let ErrorText = ({ children, ...props }) => (
-  <div className="gv-text-error" {...props}>{children}</div>
+  <div className="gv-text-error" {...props}>
+    {children}
+  </div>
 )
 
 export default ErrorText
