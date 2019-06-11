cfb58d17aaba44a2c1752e8614e61b6b926bf654
Decrapifier <decrapifier@govspend.us>
2019-06-11 17:32:49 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti

--- src/themes/greyVest/ErrorList.js
+++ src/themes/greyVest/ErrorList.js
@@ -2,10 +2,14 @@ import React from 'react'
 import _ from 'lodash/fp'
 import ErrorText from './ErrorText'
 
-let ErrorList = ({ ErrorComponent = ErrorText, children, ...props }) => _.map(
-  e => e
-    ? <ErrorComponent key={e} {...props}>{e}</ErrorComponent> 
-    : null,
-  _.castArray(children)
-)
+let ErrorList = ({ ErrorComponent = ErrorText, children, ...props }) =>
+  _.map(
+    e =>
+      e ? (
+        <ErrorComponent key={e} {...props}>
+          {e}
+        </ErrorComponent>
+      ) : null,
+    _.castArray(children)
+  )
 export default ErrorList
