9c28ed4f1fd21385c09601ea45f8ad4df1f74db5
Decrapifier <decrapifier@govspend.us>
2019-06-11 20:00:12 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti

--- src/themes/greyVest/ErrorList.js
+++ src/themes/greyVest/ErrorList.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import ErrorText from './ErrorText'
 
-let ErrorList = ({ Component = ErrorText, children, ...props }) => 
-  React.Children.map(
-    children,
-    e => 
-      e ? (
-        <Component key={e} {...props}>
-          {e}
-        </Component>
-      ) : null
+let ErrorList = ({ Component = ErrorText, children, ...props }) =>
+  React.Children.map(children, e =>
+    e ? (
+      <Component key={e} {...props}>
+        {e}
+      </Component>
+    ) : null
   )
 export default ErrorList
