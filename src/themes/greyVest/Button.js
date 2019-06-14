e441d5ea67c5f7ed0ecff4c1a989832429f9b359
Samuel Greene <sam@samuelgreene.info>
2019-06-13 22:44:46 -0400

    Button: don't do onClick when disabled

--- src/themes/greyVest/Button.js
+++ src/themes/greyVest/Button.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
-let Button = ({ isActive, primary, as: As = 'button', className, ...x }) => (
+let Button = ({ isActive, primary, as: As = 'button', className, disabled, onClick, ...x }) => (
   <As
     className={`gv-button ${isActive ? 'active' : ''} ${
       primary ? 'primary' : ''
     } ${className || ''}`}
+    disabled={disabled}
+    onClick={disabled ? () => {} : onClick}
     {...x}
   />
 )
