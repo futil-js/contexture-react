ecdb2283bbb7e135c687fbe3d83f61bd59dd1469
Decrapifier <decrapifier@govspend.us>
2020-01-30 03:55:15 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti

--- src/stories/imdb/imdb.stories.js
+++ src/stories/imdb/imdb.stories.js
@@ -3,7 +3,10 @@ import ThemePicker from '../themePicker'
 import ResultComponentStory from './resultComponents'
 import SearchLayoutStory from './searchLayout'
 
-export default { title: 'Live Demos | IMDB Search', decorators: [ThemePicker('greyVest')] }
+export default {
+  title: 'Live Demos | IMDB Search',
+  decorators: [ThemePicker('greyVest')],
+}
 
 export let customResultComponents = () => <ResultComponentStory />
 
