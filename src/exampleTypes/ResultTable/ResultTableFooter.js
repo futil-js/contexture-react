74d689c7c20c50260f4a3c56cef7b6c9e8aefe3c
Michelle Saad <git@michsa.me>
2020-02-12 13:08:41 -0500

    ResultTableFooter: use GV TableFooter

--- src/exampleTypes/ResultTable/ResultTableFooter.js
+++ src/exampleTypes/ResultTable/ResultTableFooter.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import F from 'futil'
 import _ from 'lodash/fp'
-import { Grid, GridItem, PagerItem } from 'grey-vest'
-import ResultPager from '../ResultPager'
+import { TableFooter } from 'grey-vest'
 import { contexturifyWithoutLoader } from '../../utils/hoc'
 
 let getTotalRecords = F.cascade([
@@ -10,78 +9,20 @@ let getTotalRecords = F.cascade([
   'context.totalRecords',
 ])
 
-let PageSize = ({
-  value,
-  onChange = () => {},
-  sizeOptions = [20, 50, 100, 250],
-}) => (
-  <div>
-    <span style={{ marginRight: 4 }}>View</span>
-    {_.map(
-      size => (
-        <PagerItem
-          style={{ margin: 2, width: 'auto', minWidth: '2em' }}
-          key={size}
-          active={size === value}
-          onClick={() => onChange(size)}
-        >
-          {size}
-        </PagerItem>
-      ),
-      _.flow(
-        _.concat(value),
-        _.sortBy(_.identity),
-        _.sortedUniq
-      )(sizeOptions)
-    )}
-  </div>
-)
-
-let ResultPageSize = contexturifyWithoutLoader(({ tree, node, ...props }) => (
-  <PageSize
-    value={node.pageSize}
-    onChange={pageSize => {
+let ResultTableFooter = ({ tree, node, pageSizeOptions }) => (
+  <TableFooter
+    page={node.page || 1}
+    onChangePage={page => tree.mutate(node.path, { page })}
+    pageSize={node.pageSize}
+    onChangePageSize={pageSize => {
       tree.mutate(node.path, {
         pageSize,
         page: _.min([node.page, _.ceil(getTotalRecords(node) / pageSize)]),
       })
     }}
-    {...props}
+    pageSizeOptions={pageSizeOptions}
+    totalRecords={getTotalRecords(node)}
   />
-))
-
-let ResultTableCount = contexturifyWithoutLoader(({ node, ...props }) => {
-  let totalRecords = getTotalRecords(node)
-  let { pageSize, page } = node
-  return (
-    <div {...props}>
-      <b>Showing</b> {pageSize * (page - 1) + 1}-
-      {_.min([totalRecords, pageSize * page])} of {totalRecords}
-    </div>
-  )
-})
-
-let ResultTableFooter = ({ tree, node, path, sizeOptions, ...props }) => (
-  <Grid
-    columns={3}
-    style={{ paddingTop: 16 }}
-    areas={['left middle right']}
-    {...props}
-  >
-    <GridItem
-      as={ResultPageSize}
-      area="left"
-      place="center start"
-      {...{ tree, node, path, sizeOptions }}
-    />
-    <ResultPager {...{ tree, node, path }} />
-    <GridItem
-      area="right"
-      as={ResultTableCount}
-      place="center end"
-      {...{ tree, node, path }}
-    />
-  </Grid>
 )
 
-export default ResultTableFooter
+export default contexturifyWithoutLoader(ResultTableFooter)
