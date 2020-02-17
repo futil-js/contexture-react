546e5e4488677b0335f6e4bef330614b700b26fd
Michelle Saad <git@michsa.me>
2020-02-17 13:34:41 -0500

    "fix" ToggleFiltersButton

--- src/purgatory/ToggleFiltersButton.js
+++ src/purgatory/ToggleFiltersButton.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import { withTheme } from '../utils/theme'
 
-let ToggleFiltersButton = ({ onClick, theme: { AlternateButton, Icon } }) => (
-  <AlternateButton title="Toggle Filters" onClick={onClick}>
-    <Icon icon="FilterAdd" />
-  </AlternateButton>
+let ToggleFiltersButton = ({ onClick, theme: { AlternateButton } }) => (
+  <AlternateButton title="Toggle Filters" onClick={onClick} icon="FilterAdd" />
 )
 export default withTheme(ToggleFiltersButton)
