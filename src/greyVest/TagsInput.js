e905a202f4f49ec8447af3c55513fbe02d92798f
Decrapifier <decrapifier@govspend.us>
2020-07-10 17:34:17 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti

--- src/greyVest/TagsInput.js
+++ src/greyVest/TagsInput.js
@@ -34,10 +34,7 @@ let TagsInput = ({
         _.difference(_, tags),
         _.map(addTag)
       )
-    : _.flow(
-        _.trim,
-        addTag
-      )
+    : _.flow(_.trim, addTag)
   return (
     <div className={'tags-input'} ref={containerRef} style={{ ...style }}>
       <Flex
